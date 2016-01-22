0fc8482737c841f54c72d17e17d9f6b63e70bfc5
karimfawzyg <karim.fawzy@live.com>
2016-01-21 21:44:11 -0500

    Update MazeImpl.java

--- MazeImpl.java
+++ MazeImpl.java
@@ -489,10 +489,10 @@ public class MazeImpl extends Maze implements Serializable, ClientListener, Runn
                         point = new Point(randomGen.nextInt(maxX),randomGen.nextInt(maxY));
                         cell = getCellImpl(point);
                 }
-                Direction d = Direction.random();
-                while(cell.isWall(d)) {
-                        d = Direction.random();
-                }
+                Direction d = Direction.North;//random();
+                //while(cell.isWall(d)) {
+                //         d = Direction.random();
+                //}
                 cell.setContents(target);
                 clientMap.put(target, new DirectedPoint(point, d));
                 update();
